8755a5df0f13648202262fecd81a9f2dd64076c6
Vofforila <polberci@yahoo.ro>
2024-11-17 21:33:09 +0200

    up table

--- javascript/home.js
+++ javascript/home.js
@@ -11,8 +11,8 @@ function filterTable(filterIndex) {
    const keys = [
       "set_image",
       "flower",
-      "feather",
-      "zonya",
+      "plume",
+      "sands",
       "goblet",
       "circlet",
       "character",
@@ -131,3 +131,199 @@ function filterTable(filterIndex) {
 
    xhr.send();
 }
+
+function testArtefact(filterIndex) {
+   const input = document.querySelectorAll(
+      '#artefactsTable input[type="text"]'
+   )[filterIndex];
+   const filter = input.value.toLowerCase().trim();
+   const table_body = document.getElementById("artefactsTableBody");
+
+   // Clear existing table rows
+   table_body.innerHTML = "";
+
+   const keys = [
+      "set_image",
+      "flower",
+      "plume",
+      "sands",
+      "goblet",
+      "circlet",
+      "character",
+      "set_type",
+   ];
+
+   function fetchData(url) {
+      return fetch(url)
+         .then((response) => {
+            if (!response.ok) {
+               throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+         })
+         .catch((error) => {
+            console.error(`Error fetching data from ${url}:`, error);
+            throw error;
+         });
+   }
+
+   fetchData("/database/artefacts.json").then((perfect_artefacts) => {
+      return fetchData("/database/test_artefacts.json").then(
+         (test_artefacts) => {
+            for (const test_artefact of test_artefacts) {
+               for (const perfect_artefact of perfect_artefacts) {
+                  if (test_artefact.setKey === perfect_artefact.set_type) {
+                     var type = test_artefact.slotKey;
+                     // console.log(type);
+                     // console.log(perfect_artefact[type]);
+
+                     var count = 0;
+
+                     const perfect_substat_types =
+                        perfect_artefact[type].split(" + ");
+
+                     var test_mainstat_type;
+                     switch (test_artefact.mainStatKey) {
+                        case "atk":
+                           test_mainstat_type = "ATK";
+                           break;
+                        case "atk_":
+                           test_mainstat_type = "ATK";
+                           break;
+                        case "hp":
+                           test_mainstat_type = "HP";
+                           break;
+                        case "hp_":
+                           test_mainstat_type = "HP";
+                           break;
+                        case "enerRech_":
+                           test_mainstat_type = "EC";
+                           break;
+                        case "eleMas":
+                           test_mainstat_type = "EM";
+                           break;
+                        case "def":
+                           test_mainstat_type = "DEF";
+                           break;
+                        case "def_":
+                           test_mainstat_type = "DEF";
+                           break;
+                        case "critDMG_":
+                           test_mainstat_type = "CRIT";
+                           break;
+                        case "critRate_":
+                           test_mainstat_type = "CRIT";
+                           break;
+                        case "heal_":
+                           test_mainstat_type = "HEAL";
+                           break;
+                        case "hydro_dmg_":
+                           test_mainstat_type = "HYDRO";
+                           break;
+                        case "electro_dmg_":
+                           test_mainstat_type = "ELECTRO";
+                           break;
+                        case "anemo_dmg_":
+                           test_mainstat_type = "ANEMO";
+                           break;
+                        case "dendro_dmg_":
+                           test_mainstat_type = "DENDRO";
+                           break;
+                        case "cryo_dmg_":
+                           test_mainstat_type = "CRYO";
+                           break;
+                        case "pyro_dmg_":
+                           test_mainstat_type = "PYRO";
+                           break;
+                        case "geo_dmg_":
+                           test_mainstat_type = "GEO";
+                           break;
+                     }
+
+                     if (perfect_substat_types[0] === test_mainstat_type) {
+                        test_artefact.substats.forEach((substat) => {
+                           var test_substat_type;
+
+                           switch (substat.key) {
+                              case "atk":
+                                 test_substat_type = "ATK";
+                                 break;
+                              case "atk_":
+                                 test_substat_type = "ATK";
+                                 break;
+                              case "hp":
+                                 test_substat_type = "HP";
+                                 break;
+                              case "hp_":
+                                 test_substat_type = "HP";
+                                 break;
+                              case "enerRech_":
+                                 test_substat_type = "EC";
+                                 break;
+                              case "eleMas":
+                                 test_substat_type = "EM";
+                                 break;
+                              case "def":
+                                 test_substat_type = "DEF";
+                                 break;
+                              case "def_":
+                                 test_substat_type = "DEF";
+                                 break;
+                              case "critDMG_":
+                                 test_substat_type = "CRIT";
+                                 break;
+                              case "critRate_":
+                                 test_substat_type = "CRIT";
+                                 break;
+                              case "heal_":
+                                 test_substat_type = "HEAL";
+                                 break;
+                              case "hydro_dmg_":
+                                 test_substat_type = "HYDRO";
+                                 break;
+                              case "electro_dmg_":
+                                 test_substat_type = "ELECTRO";
+                                 break;
+                              case "anemo_dmg_":
+                                 test_substat_type = "ANEMO";
+                                 break;
+                              case "dendro_dmg_":
+                                 test_substat_type = "DENDRO";
+                                 break;
+                              case "cryo_dmg_":
+                                 test_substat_type = "CRYO";
+                                 break;
+                              case "pyro_dmg_":
+                                 test_substat_type = "PYRO";
+                                 break;
+                              case "geo_dmg_":
+                                 test_substat_type = "GEO";
+                                 break;
+                           }
+
+                           perfect_substat_types.forEach(
+                              (perfect_substat_type) => {
+                                 if (
+                                    perfect_substat_type === test_substat_type
+                                 ) {
+                                    count++;
+                                 }
+                              }
+                           );
+                        });
+                        if (count >= 3) {
+                           console.log(test_artefact);
+                           console.log(count);
+                           count = 0;
+                           break;
+                        }
+                     }
+                  }
+               }
+            }
+         }
+      );
+   });
+}
+
+testArtefact(0);
